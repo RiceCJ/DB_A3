a0ae190df0d7992a51333f78a78a8b999c47e30c
cj <ricezcj@gmail.com>
2017-02-18 09:38:51 -0600

    add some comments

--- Main/DatabaseTable/source/Sorting.cc
+++ Main/DatabaseTable/source/Sorting.cc
@@ -10,11 +10,59 @@
 
 using namespace std;
 
-void mergeIntoFile (MyDB_TableReaderWriter &, vector <MyDB_RecordIteratorAltPtr> &, function <bool ()>, MyDB_RecordPtr, MyDB_RecordPtr) {}
+/**
+ * used for merge pahse
+ * tbl: merge into this table
+ * itervec: vector of record iterators
+ * comp: comparator function
+ * rec1 & rec2, used to compare
+ */
+void mergeIntoFile (MyDB_TableReaderWriter &tbl, vector <MyDB_RecordIteratorAltPtr> &itervec,
+					function <bool ()> comp, MyDB_RecordPtr rec1, MyDB_RecordPtr rec2) {
 
-vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr, MyDB_RecordIteratorAltPtr, MyDB_RecordIteratorAltPtr, function <bool ()>, 
-	MyDB_RecordPtr, MyDB_RecordPtr) {return vector <MyDB_PageReaderWriter> (); } 
-	
-void sort (int, MyDB_TableReaderWriter &, MyDB_TableReaderWriter &, function <bool ()>, MyDB_RecordPtr, MyDB_RecordPtr) {} 
+
+
+
+}
+
+/**
+ * used for sort pahse
+ * mngr: the buffer manager
+ * iter1:
+ * iter2:
+ * comp: comparator function
+ * rec1 & rec2, used to compare
+ */
+vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr mngr,
+											  MyDB_RecordIteratorAltPtr iter1,
+											  MyDB_RecordIteratorAltPtr iter2,
+											  function <bool ()> comp,
+											  MyDB_RecordPtr rec1,
+											  MyDB_RecordPtr rec2) {
+	vector <MyDB_PageReaderWriter> mergedvec;
+
+
+
+	return mergedvec;
+
+}
+
+/**
+ * TPMMS algorithm
+ * r: run size
+ * tblread: table to be sorted
+ * tbwrite: table sorted
+ * comp: comparator function
+ * rec1 & rec2, used to compare
+ */
+void sort (int r, MyDB_TableReaderWriter &tblread, MyDB_TableReaderWriter &tblwrite,
+		   function <bool ()> comp, MyDB_RecordPtr rec1, MyDB_RecordPtr rec2) {
+
+
+
+
+
+
+}
 
 #endif
